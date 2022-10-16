e9c059d299c8635706009f6b5e330148ac311a48
Guang-Yu6 <everyting-day@foxmail.com>
2022-10-16 15:03:42 +0800

    完成四个基本页面

--- src/components/welcome/forth.tsx
+++ src/components/welcome/forth.tsx
@@ -1,10 +1,25 @@
 import {defineComponent} from "vue";
+import s from './First.module.scss'
+import icon from '../../assets/icons/cloud.svg'
+import {RouterLink} from "vue-router";
 
 export const Forth = defineComponent({
-    setup:(props, context)=>{
+    setup: () => {
         return () => (
-            <div> 第3个 </div>
+            <div class={s.wrapper}>
+
+                <div class={s.card}>
+                    <img src={icon}/>
+                    <h2>每日提醒 <br/>还会省钱</h2>
+                </div>
+
+                <div class={s.actions}>
+                    <RouterLink class={s.fake} to="start">跳过</RouterLink>
+                    <RouterLink to="/welcome/1">完成</RouterLink>
+                    <RouterLink class={s.fake} to="start">跳过</RouterLink>
+                </div>
+
+            </div>
         )
     }
 })
-
